4ad7af620087b913579f328e3039f06e9c7b02d5
Ank646 <96883697+Ank646@users.noreply.github.com>
2024-09-04 19:35:23 +0530

    first commit

---
+++ Sorting_Visualizer-master/scripts/bubble_sort.js
@@ -0,0 +1,56 @@
+/*
+    *****************
+    DONE BY:-   TUMMALA KETHAN
+    
+    *****************
+*/
+function Bubble()
+{
+    //Setting Time complexities
+    document.getElementById("Time_Worst").innerText="O(N^2)";
+    document.getElementById("Time_Average").innerText="Θ(N^2)";
+    document.getElementById("Time_Best").innerText="Ω(N)";
+
+    //Setting Space complexity
+    document.getElementById("Space_Worst").innerText="O(1)";
+    let startTime = performance.now();
+
+   
+    c_delay=0;
+
+    for(var i=0;i<array_size-1;i++)
+    {
+        for(var j=0;j<array_size-i-1;j++)
+        {
+            div_update(divs[j],div_sizes[j],"yellow");//Color update
+
+            if(div_sizes[j]>div_sizes[j+1])
+            {
+                div_update(divs[j],div_sizes[j], "red");//Color update
+                div_update(divs[j+1],div_sizes[j+1], "red");//Color update
+
+                var temp=div_sizes[j];
+                div_sizes[j]=div_sizes[j+1];
+                div_sizes[j+1]=temp;
+
+                div_update(divs[j],div_sizes[j], "red");//Height update
+                div_update(divs[j+1],div_sizes[j+1], "red");//Height update
+            }
+            div_update(divs[j],div_sizes[j], "#3498db");//Color updat
+        }
+        div_update(divs[j],div_sizes[j], "green");//Color update
+    }
+    div_update(divs[0],div_sizes[0], "green");//Color update
+    let endTime = performance.now();
+    let timeTaken = ((endTime - startTime) / 1000).toFixed(4);
+    document.getElementById("Time_Taken").innerText = ` ${timeTaken} seconds`;
+
+    enable_buttons();
+}
+
+/*
+    *****************
+    DONE BY:-   TUMMALA KETHAN
+    
+    *****************
+*/
